6d6f7256d0b160013fd213c422cd350066b53cac
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-07-23 00:05:46 +0200

    add

--- frontend/src/components/tweetElement/TweetElement.tsx
+++ frontend/src/components/tweetElement/TweetElement.tsx
@@ -1,6 +1,6 @@
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoMdHeart } from "react-icons/io";
-
+import Image from "next/image";
 import { tweetData } from "@/mocks/tweet";
 import axios from "axios";
 
@@ -20,12 +20,12 @@ const TweetElement = (props: { data: tweetData; fetchTweets: any }) => {
   return (
     <div className="border p-2 flex flex-col  md:h-fit md:w-[400px] md:rounded">
       <div className="flex items-center gap-4">
-        <img
-          src="https://www.freeiconspng.com/thumbs/profile-icon-png/profile-icon-9.png"
+        <Image
+          src="https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611746.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1721606400&semt=ais_user"
           width={65}
           height={65}
           alt="Immagine del profilo"
-          className="border rounded-full p-2"
+          className="border rounded-full "
         />
         <h3 className="italic text-gray-500">@{data.user}</h3>
         <div className="flex flex-col items-center ml-auto ">
