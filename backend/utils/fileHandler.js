680cc15847852d4117de570bca599ec3fc8dcc40
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-07-24 01:21:11 +0200

    vercel opciones

--- backend/utils/fileHandler.js
+++ backend/utils/fileHandler.js
@@ -4,17 +4,17 @@ const path = require("path");
 const FILE_PATH = path.join(__dirname, "..", "tweets.json");
 
 // CARICA I TWEET DAL FILE
-const loadTweets = async () => {
-  try {
-    const data = await fs.readFile(FILE_PATH, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      return [];
-    }
-    throw error;
-  }
-};
+// const loadTweets = async () => {
+//   try {
+//     const data = await fs.readFile(FILE_PATH, "utf8");
+//     return JSON.parse(data);
+//   } catch (error) {
+//     if (error.code === "ENOENT") {
+//       return [];
+//     }
+//     throw error;
+//   }
+// };
 
 // SALVA I TWEET NEL FILE
 const saveTweets = async (tweets) => {
